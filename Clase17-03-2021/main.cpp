f91df8c015b3358f56953c74c618bfdcdb9f597d
Carlosgdc <cdiazca@unal.edu.co>
2021-04-12 18:58:39 -0500

    Lo que realizamos fue organizar el ejercicio que hicimos en el anterior commit para que quedara modularizado en funciones y no todo en la función main.

--- Clase17-03-2021/main.cpp
+++ Clase17-03-2021/main.cpp
@@ -4,30 +4,35 @@
 //
 //  Created by Countyfoot on 4/12/21.
 //
-// En este programa resolveremos un ejercicio el cual consiste en expresar la suma de los numeros del 1 al 1000 de aquellos que sean multiplos de 3 y 5.
+// En este programa organizaremos el ejercicio anterior modularizandolo en funciones.
 //
 //Comenzamos añadiendo las librerias.
 #include <iostream>
 #include <cstdlib>
 
+void print_sum_multiples(int m, int n);
 
 int main(int argc, char *argv[])
 {
     int a = std::atof(argv[1]);
     int b = std::atof(argv[2]);
     
-    int suma=0;
-    for(int ii=a; ii<b; ++ii)
-    {
-        if(ii%3==0 || ii%5==0)
-        {
-            suma+=ii;
-        }
-    }
-    std::cout<<"La suma es: "<<"\n";
-    
+    print_sum_multiples(a, b);
     return 0;
 }
+void print_sum_multiples(int m, int n)
+{
+    int suma=0;
+    for(int ii=m; ii<n; ++ii)
+        {
+            if(ii%3==0 || ii%5==0)
+                {
+                    suma+=ii;
+                }
+        }
+    std::cout<<"La suma es: "<<"\n";
+}
+
 // El programa esta hecho, si al momento de compilar y luego al ejecutar escribimos:
 // ./a.out 1 1000
 // El programa nos botara la suma de: 233168.
